144f7d97e83def3b39e8d095c6ca1fd49ce2671d
Jason Ibrahim <jibrahim@opentext.com>
2017-01-17 13:44:09 -0800

    added setHeaderButtons method to AWHeaderBar class

--- src/plugins/header.ts
+++ src/plugins/header.ts
@@ -3,6 +3,10 @@ import {AWProxy} from '../proxy';
 
 export class AWHeaderBar extends AWPlugin {
 
+    // TODO needs documentation
+    ButtonName = {LeftOne: 0, LeftTwo: 1, RightOne: 2, RightTwo: 3};
+    // TODO needs documentation
+    ButtonImage = {Hamburger: 0, Back: 1, Settings: 2, Appmenu: 3, None: 5, Dots: 6, Search: 7};
     private callback: any;
 
     setHeader(config: any) {
@@ -30,6 +34,18 @@ export class AWHeaderBar extends AWPlugin {
             []
         );
     }
+
+    // TODO needs documentation
+    setHeaderButtons(config: any) {
+        AWProxy.exec(
+            (() => this.successHandler)(),
+            (() => this.errorHandler)(),
+            'AWHeaderBar',
+            'setHeaderButtons',
+            [config]
+        );
+    }
 }
 // alias
-export class AWHeader extends AWHeaderBar {}
+export class AWHeader extends AWHeaderBar {
+}
