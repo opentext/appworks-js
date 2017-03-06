c3578e9a6a26c1dd87c7c4930ba504e1d3fbd5e3
Jason Ibrahim <jibrahim@opentext.com>
2017-03-06 14:26:00 -0800

    modify menu plugin to support adding menu sections to desktop menu

--- src/plugins/menu.ts
+++ src/plugins/menu.ts
@@ -13,6 +13,28 @@ export class AWMenu extends AWPlugin {
         );
     }
 
+    addSectionsToMenu(menuSections: MenuSection[]) {
+        return new Promise((resolve, reject) => {
+            AWProxy.exec(
+                resolve,
+                reject,
+                'AWMenu',
+                'addSectionsToMenu',
+                [menuSections]
+            );
+        });
+    }
+
+    didOpenMenuItem(callback: any) {
+        AWProxy.exec(
+            callback,
+            (() => this.errorHandler)(),
+            'AWMenu',
+            'receive',
+            []
+        );
+    }
+
     openListener(listener: any) {
         AWProxy.exec(
             listener,
@@ -26,4 +48,14 @@ export class AWMenu extends AWPlugin {
     didTapMenuItem(listener: any) {
         return this.openListener(listener);
     }
+}
+
+export interface MenuSection {
+    subhead: string;
+    items: MenuItem[];
+}
+
+export interface MenuItem {
+    title: string;
+    action: any;
 }
