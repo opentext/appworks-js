ddef8c076e2ea64a4b1439b4603fd314174fbe65
Nick Deakin <ndeakin@opentext.com>
2019-01-28 10:30:20 +0000

    OAG-8811 - Added getAppName() function to AWAppManager plugin so an app can retrieve their own name

--- src/plugins/app-manager/app-manager.d.ts
+++ src/plugins/app-manager/app-manager.d.ts
@@ -1,4 +1,5 @@
 import { AWPlugin } from '../../common/plugin';
 export declare class AWAppManager extends AWPlugin {
     closeActiveApp(): void;
+    getAppName(): void;
 }
--- src/plugins/app-manager/app-manager.ts
+++ src/plugins/app-manager/app-manager.ts
@@ -2,14 +2,23 @@ import {AWPlugin} from '../../common/plugin';
 import {AWProxy} from '../../common/proxy';
 
 export class AWAppManager extends AWPlugin {
-  closeActiveApp() {
-    AWProxy.exec(
-      (() => this.successHandler)(),
-      (() => this.errorHandler)(),
-      'AWAppManager',
-      'closeActiveApp',
-      []
-    );
-  }
+    closeActiveApp() {
+        AWProxy.exec(
+            (() => this.successHandler)(),
+            (() => this.errorHandler)(),
+            'AWAppManager',
+            'closeActiveApp',
+            []
+        );
+    }
+    getAppName() {
+        AWProxy.exec(
+            (() => this.successHandler)(),
+            (() => this.errorHandler)(),
+            'AWAppManager',
+            'getAppName',
+            []
+        );
+    }
 }
 
