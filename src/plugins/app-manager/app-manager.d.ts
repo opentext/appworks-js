4d272e467ea993b7de0b002bd103b7424f4f7357
Nick Deakin <ndeakin@opentext.com>
2019-09-09 12:01:18 +0100

    OTAG-9344 - Added isFirstRun and setAppHasRun to AWAppManager (also added appVersion). This is to replace shouldClearCache and resetShouldClearCache, but as D2 are stuck in a code freeze, these are aliased for now)

--- src/plugins/app-manager/app-manager.d.ts
+++ src/plugins/app-manager/app-manager.d.ts
@@ -2,6 +2,9 @@ import { AWPlugin } from '../../common/plugin';
 export declare class AWAppManager extends AWPlugin {
     closeActiveApp(): void;
     getAppName(): void;
+    getAppVersion(success: any, error: any): void;
+    isFirstRun(success: any, error: any): void;
+    setAppHasRun(success: any, error: any): void;
     resetShouldClearCache(): void;
     shouldClearCache(success: any): void;
 }
